9fa25053d4321651cab2fb37ff1f6c962c19ba5d
garrettvt <garrettvt1@yahoo.com>
2022-03-11 20:14:30 -0800

    Blank, pic1, and pic2 headers in calibration folder

--- Raspberry Pi/calibration/blankPic.py
+++ Raspberry Pi/calibration/blankPic.py
@@ -1,3 +1,11 @@
+# Kellen Hartnett
+# Adrian Lewis
+# Connor Lowe
+# Sahibjeet Singh
+# Garrett Tashiro
+# EE 475, Group 5 Capstone Project
+# For taking a blank picture of the chess board
+
 import os
 
 os.system("fswebcam -r 1920x1080 /home/pi/Embedded-Capstone/calibration/blank1.bmp")
--- Raspberry Pi/calibration/takePic.py
+++ Raspberry Pi/calibration/takePic.py
@@ -1,3 +1,11 @@
+# Kellen Hartnett
+# Adrian Lewis
+# Connor Lowe
+# Sahibjeet Singh
+# Garrett Tashiro
+# EE 475, Group 5 Capstone Project
+# For taking the first picture of the chess board after a move
+
 import os
 
 os.system("fswebcam -r 1920x1080 /home/pi/Embedded-Capstone/camCal/calibration/test1.bmp")
